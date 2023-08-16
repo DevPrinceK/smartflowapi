00fbfc7bda830ccbeb97a1c17cc789aa4d4fa457
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-16 17:57:34 +0000

    Create firebase_config.py

---
+++ core/api/firebase_config.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyD4V-9krwKA3CpFj5Vt6zTdBofkV908eRc",
+  "authDomain": "smartflow-f427f.firebaseapp.com",
+  "databaseURL": "https://smartflow-f427f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "smartflow-f427f",
+  "storageBucket": "smartflow-f427f.appspot.com",
+  "messagingSenderId": "517900042843",
+  "appId": "1:517900042843:web:e56889e88715a0d113757e"
+};
