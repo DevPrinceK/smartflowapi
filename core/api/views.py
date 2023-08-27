1d38c3aedad105c181a220a1721e3b008fcae719
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-27 19:13:42 +0000

    Update views.py

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,6 @@
+import random
+import time
+from datetime import datetime
 from django.shortcuts import render
 
 # import restapi essentials
@@ -8,14 +11,14 @@ from rest_framework.response import Response
 # firebase realtime database config
 import pyrebase
 
-from api.models import Humidity, Moisture, Temperature
-from api.serializers import HumiditySerializer, MoistureSerializer, TemperatureSerializer
+from api.models import EnvironmentalData, Humidity, Moisture, Temperature
+from api.serializers import EnvironmentalDataSerializer, HumiditySerializer, MoistureSerializer, TemperatureSerializer
+from core.utils.utilities import generate_data
 from .firebase_config import firebaseConfig
 
 # initialize firebase realtime database
 firebase = pyrebase.initialize_app(firebaseConfig)
 database = firebase.database()
-
 
 
 class OVerviewAPIView(APIView):
@@ -27,14 +30,19 @@ class OVerviewAPIView(APIView):
         return Response({
             "message": "Welcome to the API for the Smart Farming Project.",
             "endpoints": [
-                {"endpoint": "all-data", "description": "Get all the data from the database"},
-                {"endpoint": "humidity", "description": "Get all the humidity data from the database"},
-                {"endpoint": "temperature", "description": "Get all the temperature data from the database"},
-                {"endpoint": "moisture", "description": "Get all the moisture data from the database"},
-                {"endpoint": "populate-db", "description": "Fetch data from the firebase realtime database and save it to the django database"},
-                ],
+                {"endpoint": "all-data",
+                    "description": "Get all the data from the database"},
+                {"endpoint": "humidity",
+                    "description": "Get all the humidity data from the database"},
+                {"endpoint": "temperature",
+                    "description": "Get all the temperature data from the database"},
+                {"endpoint": "moisture",
+                    "description": "Get all the moisture data from the database"},
+                {"endpoint": "populate-db",
+                    "description": "Fetch data from the firebase realtime database and save it to the django database"},
+            ],
             "url_format": "http://127.0.0.1:8000/api-v1/<endpoint>/",
-           },
+        },
             status=status.HTTP_200_OK
         )
 
@@ -45,40 +53,31 @@ class FetchDataAPIView(APIView):
     and saves it to the django database
     '''
     permission_classes = [permissions.AllowAny]
-    
+
     def post(self, request, *args, **kwargs):
         '''For fetching data from the firebase realtime database'''
-        humidity = database.child("bulkHumidity").get().val()
-        moisture = database.child("bulkMoisture").get().val()
-        temperature = database.child("bulkTemperature").get().val()
-        hum_list = []
-        temp_list = []
-        moist_list = []
-        for item in dict(humidity):
-            if Humidity.objects.filter(date=item).exists():
+        all_data = dict(database.child("data").get().val())
+        all_data_values = list(all_data.values())
+        all_data_list = []
+
+        for item in all_data_values:
+            stamp = datetime.fromtimestamp(item['timestamp'])
+            if EnvironmentalData.objects.filter(timestamp=stamp).exists():
                 continue
-            hum_list.append(Humidity(humidity=humidity[item], date=item))
-            
-        for item in dict(moisture):
-            if Moisture.objects.filter(date=item).exists():
-                continue
-            moist_list.append(Moisture(moisture=moisture[item], date=item))
-            
-        for item in dict(temperature):
-            if Temperature.objects.filter(date=item).exists():
-                continue
-            temp_list.append(Temperature(temperature=temperature[item], date=item))
-        
-        # do bulk create to save multiple objects at once
-        print("running bulk_create command...")
-        Humidity.objects.bulk_create(hum_list)
-        Moisture.objects.bulk_create(moist_list)
-        Temperature.objects.bulk_create(temp_list)
-        
-        print("fetching data...")
+            else:
+                all_data_list.append(
+                    EnvironmentalData(
+                        humidity=item['humidity'],
+                        temperature=item['temperature'],
+                        moisture=item['moisture'],
+                        timestamp=stamp,
+                    )
+                )
+        # save the data to the database all at once
+        EnvironmentalData.objects.bulk_create(all_data_list)
         return Response({
             "message": "Database Updated With More Data",
-            },
+        },
             status=status.HTTP_200_OK
         )
 
@@ -88,14 +87,10 @@ class AllDataAPIView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request):
-        all_humidity = Humidity.objects.all().order_by('-id')
-        all_temperature = Temperature.objects.all().order_by('-id')
-        all_moisture = Moisture.objects.all().order_by('-id')
-        print("getting all data...")
+        all_data = EnvironmentalData.objects.all().order_by("-id")
         return Response({
-            "humidity": HumiditySerializer(all_humidity, many=True).data,
-            "temperature": TemperatureSerializer(all_temperature, many=True).data,
-            "moisture": MoistureSerializer(all_moisture, many=True).data, },
+            "data": EnvironmentalDataSerializer(all_data, many=True).data,
+        },
             status=status.HTTP_200_OK
         )
 
@@ -105,35 +100,57 @@ class HumidityAPIView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request):
-        all_humidity = Humidity.objects.all().order_by('-id')
-        print("getting humidity data...")
+        all_data = EnvironmentalData.objects.all().order_by("-id")
+        all_humidity = [{"humidity": i.humidity, "timestamp": i.timestamp,
+                         "created_at": i.created_at} for i in all_data]
         return Response({
-            "humidity": HumiditySerializer(all_humidity, many=True).data, },
+            "humidity": all_humidity},
             status=status.HTTP_200_OK
         )
-        
+
 
 class TemperatureAPIView(APIView):
     '''For getting temperature data from the database'''
     permission_classes = [permissions.AllowAny]
 
     def get(self, request):
-        all_temperature = Temperature.objects.all().order_by('-id')
-        print("getting temperature data...")
+        all_data = EnvironmentalData.objects.all().order_by("-id")
+        all_temperature = [{"temperature": i.temperature, "timestamp": i.timestamp,
+                            "created_at": i.created_at} for i in all_data]
         return Response({
-            "temperature": TemperatureSerializer(all_temperature, many=True).data, },
+            "temperature": all_temperature, },
             status=status.HTTP_200_OK
         )
-        
-        
+
+
 class MoistureAPIView(APIView):
     '''For getting moisture data from the database'''
     permission_classes = [permissions.AllowAny]
 
     def get(self, request):
-        all_moisture = Moisture.objects.all().order_by('-id')
-        print("getting moisture data...")
+        all_data = EnvironmentalData.objects.all().order_by("-id")
+        all_moisture = [{"moisture": i.moisture, "timestamp": i.timestamp,
+                         "created_at": i.created_at} for i in all_data]
         return Response({
-            "moisture": MoistureSerializer(all_moisture, many=True).data, },
+            "moisture": all_moisture, },
             status=status.HTTP_200_OK
         )
+
+
+class GenerateRandomEnvData(APIView):
+    '''Use to generate random environmental data'''
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            generate_data(frequency=20)
+        except Exception as er:
+            print(er)
+            return Response({
+                "message": str(er),
+            }, status=status.HTTP_417_EXPECTATION_FAILED)
+        else:
+            print("Data Successfully Generated")
+            return Response({
+                "message": "Data Successfully Generated",
+            }, status=status.HTTP_200_OK)
